8ccd7f305d4127feecae04d89e13885170b3f836
Muhammad Abu Yousof <ce.yousuf@gmail.com>
2025-06-26 12:17:12 +0600

    this is second change

--- MY_INFO/frmMyInfo.cs
+++ MY_INFO/frmMyInfo.cs
@@ -17,6 +17,7 @@ namespace MY_INFO
             InitializeComponent();
         }
 // this is test for update
+// this is second change
         private void btnConvert_Click(object sender, EventArgs e)
         {
             SecretKey secretkey = new SecretKey();
